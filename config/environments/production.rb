1c634ff56f82bcfabfa8eb97f967ccb1aa8c7293
hacdeddt <hacdeddt3@gmail.com>
2018-06-28 15:46:39 +0700

    edit enviroments production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,8 +89,7 @@ require 'base64'
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-  config.action_mailer.default_url_options = { :host => 'https://morning-eyrie-31595.herokuapp.com' }
-  Rails.application.routes.default_url_options[:host] = 'https://morning-eyrie-31595.herokuapp.com'
+  config.action_mailer.default_url_options = { :host => 'young-plateau-76500.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
     config.action_mailer.perform_deliveries = true
     config.action_mailer.raise_delivery_errors = false
@@ -99,10 +98,10 @@ require 'base64'
     config.action_mailer.smtp_settings = {
       :address            => 'smtp.gmail.com',
       :port               => 587,
-      :domain             => 'https://morning-eyrie-31595.herokuapp.com',
+      :domain             => 'young-plateau-76500.herokuapp.com',
       :authentication     => :plain,
-      :user_name          => ENV["hequanlysinhvienact@gmail.com"],
-      :password           => ENV[i],
+      :user_name          => ENV["GMAIL_USERNAME"],
+      :password           => ENV["GMAIL_PASSWORD"],
       :enable_starttls_auto => true
     }
 end
